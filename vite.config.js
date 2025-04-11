8e5bcc4ace05ea4487260b423daf24f10423e91d
Krishnansh5 <5272.krishnansh@gmail.com>
2025-04-11 12:01:31 +0530

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/Perfect-Hash-Visualiser/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    },
+    outDir: 'dist',
+    emptyOutDir: true
+  },
 })
